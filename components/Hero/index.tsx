df66c5703faca358ec78800469a210147230f8a4
cwrsiqueira <cwrsiqueira@msn.com>
2022-09-22 06:37:07 -0300

    Paleativo. Resolver bug do bg-color botões

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -17,7 +17,7 @@ const Hero = ({ mainTitle, subTitle, subTitleColor, description, btnText, btnCol
                 <div className="font-bold text-5xl text-[#1A202C] mb-6">{mainTitle}</div>
                 <div className={`font-bold text-5xl text-[${subTitleColor}] mb-6`}>{subTitle}</div>
                 <div className="font-normal text-2x1 text-[#718094] mb-6">{description}</div>
-                <div className={`font-bold text-x1 text-white bg-[${btnColor}] px-[60px] py-[25px] rounded-md ease-in-out duration-300 cursor-pointer w-[364px] m-auto hover:scale-105`}>
+                <div className={`font-bold text-x1 text-white bg-[${btnColor || '#03A9F4'}] px-[60px] py-[25px] rounded-md ease-in-out duration-300 cursor-pointer w-[364px] m-auto hover:scale-105`}>
                     <Link href={btnLink}>
                         <a>{btnText}</a>
                     </Link>
